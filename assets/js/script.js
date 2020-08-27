dcad43f493cc71b962889a64953593bb931e0176
Jrsparl <john@sparlingmusic.com>
2020-08-26 20:47:40 -0400

    Added delete task functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var taskIdCounter = 0;
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
+var pageContentEl = document.querySelector("#page-content");
 
 var taskFormHandler = function(event) {
   
@@ -92,8 +93,26 @@ return actionContainerEl;
 
 };
 
+var taskButtonHandler = function(event) {
+  console.log(event.target);
+ 
+  if (event.target.matches(".delete-btn")) {
+    var taskId = event.target.getAttribute("data-task-id");
+    deleteTask(taskId);
+  }
+
+};
 
 
-  formEl.addEventListener("submit", taskFormHandler);
+var deleteTask = function(taskId) {
+  var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+  taskSelected.remove();
+};
+
+
+
+  
+pageContentEl.addEventListener("click", taskButtonHandler);
+formEl.addEventListener("submit", taskFormHandler);
 
 
